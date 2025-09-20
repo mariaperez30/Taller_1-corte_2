877c4250187cfe1966f4c44f8ec50c7e5e832418
Maria alejandra perez hernandez <mariagale2468@gmail.com>
2025-09-19 19:30:06 -0500

    logion so so and folder services and providers

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  standalone: false
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  public name!: FormControl;
+  public lastName!: FormControl;
+  public email!: FormControl;
+  public password!: FormControl;
 
-  ngOnInit() {
+
+
+ public loginForm!:FormGroup;
+
+  constructor() {
+    this.initForm();
+   }
+
+  ngOnInit() {}
+
+
+
+  public doLogin(){
+
+    console.log(this.loginForm.value);
+    this.loginForm.reset();
+  }
+
+
+
+  private initForm(){
+    this.name = new FormControl('',[Validators.required]);
+    this.lastName = new FormControl('',[Validators.required]);
+    this.email = new FormControl('',[Validators.required, Validators.email]);
+    this.password = new FormControl('',[Validators.required, Validators.minLength(3)]);
+
+    this.loginForm = new FormGroup({
+      name: this.name,
+      lastname: this.lastName,
+      email: this.email,
+      password: this.password,
+
+
+    })
   }
 
 }
